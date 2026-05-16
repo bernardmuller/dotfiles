7e24ad85d5cc9e7b208b4c6114f8a58571448a9a
Bernard <b.mullerjnr@gmail.com>
2026-05-16 12:26:51 +0200

    init tailscale on container

--- containers/work.nix
+++ containers/work.nix
@@ -2,13 +2,31 @@
 {
 	containers.work = {
 		autoStart = true;
+	
+		privateNetwork = true;
+		hostAddress = "10.233.1.1";
+		localAddress = "10.233.1.2";
+
 		config = { config, pkgs, ... }: {
 			users.users.bernard = {
         			isNormalUser = true;
         			extraGroups = [ "wheel" ];
-        			initialPassword = "changeme";
+				openssh.authorizedKeys.keys = [
+          				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBY26prbNVsygyO7mRAMY+Z+uqhDCdvPN65C6QfkMruB"
+        			];
       			};
 
+			services.openssh = {
+			        enable = true;
+        			settings = {
+          				PasswordAuthentication = false;
+          				PermitRootLogin = "no";
+        			};
+      			};
+
+			services.tailscale.enable = true;
+
+      			networking.firewall.allowedTCPPorts = [ 22 ];
 
 			system.stateVersion = "25.11";
 		};
--- modules/hyprland/hypridle.nix
+++ modules/hyprland/hypridle.nix
@@ -17,7 +17,7 @@
 				{
 					timeout = 600;
 					on-timeout = "hyprctl dispatch dpms off";
-					on-resume = "hyprctl dispatch dpms on && hyprctl reload";
+					on-resume = "hyprctl dispatch dpms on";
 				}
 				{
 					timeout = 1800;
