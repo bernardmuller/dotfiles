0ac41a6706acbe131a7da0a6b2fa90ef084277b4
Bernard <b.mullerjnr@gmail.com>
2026-05-19 10:35:49 +0200

    claude code in LT

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -6,29 +6,44 @@
     setAsDefaultBrowser = true;
 
     profiles.default = {
+	settings = {
+        	"zen.workspaces.continue-where-left-off" = true;
+        	"zen.view.compact.hide-tabbar" = true;
+        	"zen.urlbar.behavior" = "float";
+      	};
+
+	search = {
+        	force = true;
+        	default = "ddg";
+      	};
+
       spacesForce = true;  
       spaces = {
         "Work" = {
           id = "cdd10fab-4fc5-494b-9041-325e5759195b";
           position = 2000;
           icon = "💼";
-          theme = {
-            type = "gradient";
-            colors = [{
-              red = 100; green = 150; blue = 200;
-              algorithm = "floating";
-              type = "explicit-lightness";
-              lightness = 50;
-            }];
-            opacity = 0.8;
-            texture = 0.5;
-          };
-        };
+	};
       };
 
      	mods = [
      		"ecda11ae-d3fd-4052-8881-303b2504e3ce" 
 	];
+
+	userChrome = ''
+    #navigator-toolbox {
+      background-color: #2b2b2b;
+    }
+
+    #TabsToolbar {
+      min-height: 28px;
+    }
+
+    .tab-icon-image {
+      width: 16px;
+      height: 16px;
+    }
+  '';
     };
   };
 }
--- projects/lt/flake.nix
+++ projects/lt/flake.nix
@@ -1,20 +1,24 @@
 {
   description = "LT environment";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+          "claude-code"
+        ];
+      };
     in {
       devShells.${system}.default = pkgs.mkShell {
         name = "lt";
         packages = with pkgs; [
-		google-cloud-sdk
-		nodejs_22
-          	pnpm
-          	pkg-config
+          google-cloud-sdk
+          nodejs_22
+          pnpm
+          pkg-config
+          claude-code
         ];
         shellHook = ''
           source /etc/dotfiles/lib/project-identity.sh
